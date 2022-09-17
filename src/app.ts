62143cab5aecd395da231efa57de22beddd2868e
Andrea <andrea.barberio97@gmail.com>
2022-09-17 18:53:32 +0200

    Add Validation for POST /planets Route

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,52 @@
 import express from "express";
 import "express-async-errors";
 import prisma from "./lib/prisma/client";
+
+import {
+    validate,
+    validationErrorMiddleware,
+    planetSchema,
+    PlanetData,
+} from "./lib/validation";
+
 const app = express();
-app.use(express.json())
+
+app.use(express.json());
 
 app.get("/planets", async (request, response) => {
     const planets = await prisma.planet.findMany();
 
     response.json(planets);
 });
-app.post("/planets", async (request, response) => {
-    const planet = request.body;
+app.post(
+    "/planets",
+    validate({ body: planetSchema }),
+    async (request, response) => {
+        const planet: PlanetData = request.body;
 
-    response.status(201).json(planet);
-});
+        response.status(201).json(planet);
+    }
+);
 
+app.use(validationErrorMiddleware);
 // Request examples
 
-// //GET /planets  - retrieve all the planets
+// !GET /planets  - retrieve all the planets
 // app.get("/planets", (request, response) => {});
 
-// //GET /planets/:id   -retrieve a specific planet
+// !GET /planets/:id   -retrieve a specific planet
 // app.get("/planet/:id", (request, response) => {});
 
-// //POST /planets   -Create a new planet
+// !POST /planets   -Create a new planet
 // app.post("/planets", (request, response) => {});
 
-// //PUT /planets/:id   -Replace an existing planet
+// !PUT /planets/:id   -Replace an existing planet
 // app.put("/planets/:id", (request, response) => {});
 
-// //DELETE /planets/:id    - Delete an existing planet
+// !DELETE /planets/:id    - Delete an existing planet
 // app.delete("/planets/:id", (request, response) =>{});
 
-// //POST /planets/:id/photo - Add a photo for a planet
+// !POST /planets/:id/photo - Add a photo for a planet
 // app.post("/planets/:id/photo", (request, response)=>{})
 
 export default app;
