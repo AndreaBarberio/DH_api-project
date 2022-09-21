c888f69ed2502350f197038e68a9c4ebfa0687d4
Andrea <andrea.barberio97@gmail.com>
2022-09-21 14:51:52 +0200

    Update: none

--- src/app.ts
+++ src/app.ts
@@ -92,7 +92,8 @@ app.delete("/planets/:id(\\d+)", async (request, response, next) => {
     }
 });
 
-app.post("/planets/:id(\\d+)/photo",
+app.post(
+    "/planets/:id(\\d+)/photo",
     upload.single("photo"),
     async (request, response, next) => {
         console.log("request file", request.file);
