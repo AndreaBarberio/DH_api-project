e3929b9c43a30d5498ec43942bcaa169d944dcfc
Andrea <andrea.barberio97@gmail.com>
2022-09-22 11:37:38 +0200

    Add a web page for creating a planet

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -2,19 +2,26 @@ import addFormats from "ajv-formats";
 import { Validator, ValidationError } from "express-json-validator-middleware";
 import { ErrorRequestHandler } from "express";
 
-const validator = new Validator({});
+const validator = new Validator({
+    coerceTypes: true,
+});
 addFormats(validator.ajv, ["date-time"])
     .addKeyword("kind")
     .addKeyword("modifier");
 
 export const validate = validator.validate;
-export const validationErrorMiddleware:ErrorRequestHandler = (error, request, response, next) => {
-if (error instanceof ValidationError){
-    response.status(422).send({errors:error.validationErrors})
-    next()
-
-} else {
-    next(error);
-}
+export const validationErrorMiddleware: ErrorRequestHandler = (
+    error,
+    request,
+    response,
+    next
+) => {
+    if (error instanceof ValidationError) {
+        response.status(422).send({ errors: error.validationErrors });
+        next();
+    } else {
+        next(error);
+    }
 };
 export * from "./planet";
+
